cf9ee5d76dc595468100f7b780ba6e1139bb2d40
thrkrif <103479629+thrkrif@users.noreply.github.com>
2026-01-13 03:34:09 +0900

    chore: Spring Boot Project

---
+++ src/main/java/com/thock/back/api/ThockBackendApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.thock.back.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThockBackendApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThockBackendApiServerApplication.class, args);
+    }
+
+}
