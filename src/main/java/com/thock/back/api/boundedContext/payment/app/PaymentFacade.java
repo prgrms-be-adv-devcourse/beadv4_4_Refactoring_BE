8ae761b2f1b77268d6d8d56e7da6519845df7aa3
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2026-01-16 15:48:42 +0900

    feat: WalletDto추가

--- src/main/java/com/thock/back/api/boundedContext/payment/app/PaymentFacade.java
+++ src/main/java/com/thock/back/api/boundedContext/payment/app/PaymentFacade.java
@@ -1,19 +1,28 @@
 package com.thock.back.api.boundedContext.payment.app;
 
 import com.thock.back.api.boundedContext.payment.domain.PaymentMember;
+import com.thock.back.api.boundedContext.payment.domain.Wallet;
 import com.thock.back.api.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PaymentFacade {
     private final PaymentSyncMemberUseCase paymentSyncMemberUseCase;
+    private final PaymentSupport paymentSupport;
 
     @Transactional
     public PaymentMember syncMember(MemberDto member){
         return paymentSyncMemberUseCase.syncMember(member);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Wallet> findWalletByHolder(PaymentMember holder) {
+        return paymentSupport.findWalletByHolder(holder);
     }
 
 }
--- src/main/java/com/thock/back/api/boundedContext/payment/domain/Wallet.java
+++ src/main/java/com/thock/back/api/boundedContext/payment/domain/Wallet.java
@@ -1,6 +1,7 @@
 package com.thock.back.api.boundedContext.payment.domain;
 
 import com.thock.back.api.global.jpa.entity.BaseIdAndTime;
+import com.thock.back.api.shared.payment.dto.WalletDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,5 +33,17 @@ public class Wallet extends BaseIdAndTime {
         this.member = member;
         this.balance = 0L;
         this.revenue = 0L;
+    }
+
+    public WalletDto toDto(){
+        return new WalletDto(
+                getId(),
+                getMember().getId(),
+                getMember().getName(),
+                getBalance(),
+                getRevenue(),
+                getCreatedAt(),
+                getUpdatedAt()
+        );
     }
 }
