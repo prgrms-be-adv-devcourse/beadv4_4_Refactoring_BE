2c424a4ebf0c1bfe64ffde52669b57360451b3a1
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2026-01-16 15:48:24 +0900

    feat: PaymentSupport 추가
    - 각 repository에 관련 메서드 추가

--- src/main/java/com/thock/back/api/boundedContext/payment/out/WalletRepository.java
+++ src/main/java/com/thock/back/api/boundedContext/payment/out/WalletRepository.java
@@ -1,7 +1,13 @@
 package com.thock.back.api.boundedContext.payment.out;
 
+import com.thock.back.api.boundedContext.payment.domain.PaymentMember;
 import com.thock.back.api.boundedContext.payment.domain.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WalletRepository extends JpaRepository<Wallet,Long> {
+    Optional<Wallet> findByHolder(PaymentMember holder);
+
+    Optional<Wallet> findByHolderId(int holderId);
 }
