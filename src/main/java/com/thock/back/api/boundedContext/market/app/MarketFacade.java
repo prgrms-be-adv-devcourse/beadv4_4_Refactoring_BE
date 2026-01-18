b6470fb0e6c96e215cd32d8059c9def4ecaa1d97
thrkrif <103479629+thrkrif@users.noreply.github.com>
2026-01-18 18:34:41 +0900

    feat: Cart 생성, Cart, CartItem 엔티티 작성

--- src/main/java/com/thock/back/api/boundedContext/market/app/MarketFacade.java
+++ src/main/java/com/thock/back/api/boundedContext/market/app/MarketFacade.java
@@ -1,4 +1,27 @@
 package com.thock.back.api.boundedContext.market.app;
 
+import com.thock.back.api.boundedContext.market.domain.Cart;
+import com.thock.back.api.boundedContext.market.domain.MarketMember;
+import com.thock.back.api.shared.market.dto.MarketMemberDto;
+import com.thock.back.api.shared.member.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MarketFacade {
+
+    private final MarketSyncMemberUseCase marketSyncMemberUseCase;
+    private final MarketCreateCartUseCase marketCreateCartUseCase;
+    @Transactional
+    public MarketMember syncMember(MemberDto member) {
+        return marketSyncMemberUseCase.syncMember(member);
+    }
+
+    @Transactional
+    public Cart createCart(MarketMemberDto buyer) {
+        return marketCreateCartUseCase.createCart(buyer);
+    }
 }
