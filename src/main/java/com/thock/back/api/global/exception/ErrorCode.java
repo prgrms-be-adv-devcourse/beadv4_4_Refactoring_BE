636f8b8c1e01afcaa2d15e570d3a98da7d6fbbe2
thrkrif <103479629+thrkrif@users.noreply.github.com>
2026-01-19 12:10:50 +0900

    chore: application.yml에 internalBackUrl 추가, ErrorCode 추가

--- src/main/java/com/thock/back/api/global/exception/ErrorCode.java
+++ src/main/java/com/thock/back/api/global/exception/ErrorCode.java
@@ -34,7 +34,9 @@ public enum ErrorCode {
     PRODUCT_CATEGORY_REQUIRED("PRODUCT-400-4", "카테고리 설정은 필수입니다.", HttpStatus.BAD_REQUEST),
 
     // ===== 장바구니 =====
-    CART_ITEM_NOT_FOUND("CART-404-1", "장바구니에 해당 상품이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    CART_USER_NOT_FOUND("CART-404-1", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    CART_ITEM_NOT_FOUND("CART-404-2", "장바구니에 해당 상품이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    CART_PRODUCT_API_FAILED("CART-500-1", "상품 정보를 불러올 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     // ===== 주문 =====
 
     // ===== 배송 =====
