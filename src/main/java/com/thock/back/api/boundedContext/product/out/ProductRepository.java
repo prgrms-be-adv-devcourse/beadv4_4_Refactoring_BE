30df37e86995cbf2081f7052735ea409850a7619
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2026-01-20 11:40:34 +0900

    feat: stash 작업 올리기

--- src/main/java/com/thock/back/api/boundedContext/product/out/ProductRepository.java
+++ src/main/java/com/thock/back/api/boundedContext/product/out/ProductRepository.java
@@ -10,4 +10,11 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory(Category category);
+
+    // 이름에 keyword가 포함된 것 찾기 (ex: 브랜드 이름, 키보드 등등)
+    // select * from products where name like %keyword% 와 같음
+    List<Product> findByNameContaining(String keyword);
+
+    // 이름이나 설명에 keword가 포함된 것 찾기 (ex: 무접점, 적축, 갈축 등등)
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String keyword);
 }
