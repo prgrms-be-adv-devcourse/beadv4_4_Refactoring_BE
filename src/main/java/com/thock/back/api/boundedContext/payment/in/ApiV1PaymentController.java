78f0c949d41eab26d7410e3f55225fa638e0db9f
이현종 <101362306+DevelopJJong@users.noreply.github.com>
2026-01-22 01:04:09 +0900

    feat:  지갑조회, 지갑로그조회, 판매수익조회, 결제조회 api 추가

--- src/main/java/com/thock/back/api/boundedContext/payment/in/ApiV1PaymentController.java
+++ src/main/java/com/thock/back/api/boundedContext/payment/in/ApiV1PaymentController.java
@@ -1,20 +1,20 @@
 package com.thock.back.api.boundedContext.payment.in;
 
-import com.thock.back.api.boundedContext.market.in.dto.res.CartItemListResponse;
 import com.thock.back.api.boundedContext.payment.app.PaymentConfirmService;
-import com.thock.back.api.boundedContext.payment.domain.dto.PaymentConfirmRequestDto;
+import com.thock.back.api.boundedContext.payment.app.PaymentFacade;
+import com.thock.back.api.boundedContext.payment.domain.dto.request.PaymentConfirmRequestDto;
+import com.thock.back.api.boundedContext.payment.domain.dto.response.PaymentLogResponseDto;
+import com.thock.back.api.boundedContext.payment.domain.dto.response.RevenueLogResponseDto;
+import com.thock.back.api.boundedContext.payment.domain.dto.response.WalletLogResponseDto;
+import com.thock.back.api.global.security.AuthContext;
+import com.thock.back.api.shared.payment.dto.WalletDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/payments")
@@ -22,13 +22,67 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "payment-controller", description = "결제, 지갑 관련 API(지갑 조회, 로그 조회, 결제 내역 조회 등등)")
 public class ApiV1PaymentController {
     private final PaymentConfirmService paymentConfirmService;
-    //TODO: 지갑 조회 API
+    private final PaymentFacade paymentFacade;
 
-    //TODO: Wallet Log View API
+    @Operation(
+            summary = "지갑 조회",
+            description = "사용자의 지갑을 조회합니다." +
+                    "지갑 정보를 반환합니다..")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "지갑 조회 성공"),
+            @ApiResponse(responseCode = "WALLET-404-1", description = "지갑을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "WALLET-404-2", description = "이 지갑은 현재 정지 된 상태입니다.")
+    })
+    @GetMapping("internal/wallet/{memberId}")
+    public ResponseEntity<WalletDto> getWallet(@PathVariable("member-id") Long memberId) {
+        // 내부 호출용 API
+        return ResponseEntity.ok().body(paymentFacade.walletFindByMemberId(memberId));
+    }
 
-    //TODO: Payment Log view API
+    @Operation(
+            summary = "지갑 잔액 입출금 로그 조회",
+            description = "사용자의 지갑 잔액 입출금 로그를 조회합니다." +
+                    "지갑 잔액 입출금 로그를 반환합니다..")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "지갑 조회 성공"),
+            @ApiResponse(responseCode = "WALLET-404-1", description = "지갑을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "WALLET-404-2", description = "이 지갑은 현재 정지 된 상태입니다.")
+    })
+    @GetMapping("/BalanceLog")
+    public ResponseEntity<WalletLogResponseDto> getWalletLog() throws Exception {
+        Long memberId = AuthContext.memberId();
+        return ResponseEntity.ok().body(paymentFacade.getWalletLog(memberId));
+    }
 
-    //TODO: Revenue Log View API
+    @Operation(
+            summary = "지갑 잔액 입출금 로그 조회",
+            description = "사용자의 지갑 잔액 입출금 로그를 조회합니다." +
+                    "지갑 잔액 입출금 로그를 반환합니다..")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "지갑 조회 성공"),
+            @ApiResponse(responseCode = "WALLET-404-1", description = "지갑을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "WALLET-404-2", description = "이 지갑은 현재 정지 된 상태입니다.")
+    })
+    @GetMapping("/PaymentLog")
+    public ResponseEntity<PaymentLogResponseDto> getPaymentLog() throws Exception {
+        Long memberId = AuthContext.memberId();
+        return ResponseEntity.ok().body(paymentFacade.getPaymentLog(memberId));
+    }
+
+    @Operation(
+            summary = "지갑 판매수익 입출금 로그 조회",
+            description = "사용자의 지갑 판매수익 입출금 로그를 조회합니다." +
+                    "지갑 판매수익 입출금 로그를 반환합니다..")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "지갑 조회 성공"),
+            @ApiResponse(responseCode = "WALLET-404-1", description = "지갑을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "WALLET-404-2", description = "이 지갑은 현재 정지 된 상태입니다.")
+    })
+    @GetMapping("/revenueLog")
+    public ResponseEntity<RevenueLogResponseDto> getRevenueLog() throws Exception {
+        Long memberId = AuthContext.memberId();
+        return ResponseEntity.ok().body(paymentFacade.getRevenueLog(memberId));
+    }
 
     @Operation(
             summary = "결제 검증",
--- src/main/java/com/thock/back/api/boundedContext/payment/out/RevenueLogRepository.java
+++ src/main/java/com/thock/back/api/boundedContext/payment/out/RevenueLogRepository.java
@@ -3,8 +3,9 @@ package com.thock.back.api.boundedContext.payment.out;
 import com.thock.back.api.boundedContext.payment.domain.RevenueLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RevenueLogRepository extends JpaRepository<RevenueLog, Long> {
-    Optional<RevenueLog> findByWalletId(Long walletId);
+    List<RevenueLog> findByWalletId(Long walletId);
 }
